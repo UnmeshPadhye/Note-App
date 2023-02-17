e815d5facfb7df1efef1ae647e4979c4c92f1798
Unmesh Padhye <unmeshpadhye@gmail.com>
2023-02-16 17:23:12 -0800

    cors origin updated

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,16 @@ const app = express();
 app.use(bodyParser.json());
 
 // Enable CORS
-app.use(cors());
+const corsOptions = {
+    origin: 'http://13.58.13.94:3000',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+
+// app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://13.58.13.94:3000', credentials: true }));
 
 const port = 3001;
 
