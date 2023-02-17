3e370ee81bcc93cdcda814c94f23b57c0a98c5c6
Unmesh Padhye <unmeshpadhye@gmail.com>
2023-02-16 23:42:39 -0800

    change

--- backend/index.js
+++ backend/index.js
@@ -30,11 +30,6 @@ app.use(function (req, res, next) {
 // app.use(cors(corsOptions));
 // app.use(cors({ origin: 'http://13.58.13.94:3000', credentials: true }));
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.use(bodyParser.json());
 
