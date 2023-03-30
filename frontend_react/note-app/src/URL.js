a2ac7aaceffc055dafd5d20f12948ef29ec7ca47
Unmesh Padhye <unmeshpadhye@gmail.com>
2023-03-29 20:14:53 -0700

    URL changed

--- frontend_react/note-app/src/URL.js
+++ frontend_react/note-app/src/URL.js
@@ -1,4 +1,7 @@
-const BACKEND_URL = "http://3.21.34.17:3001"
-const FRONTEND_URL = "http://3.21.34.17:3000"
+const BACKEND_URL = "http://3.14.12.195:3001"
+const FRONTEND_URL = "http://3.14.12.195:3000"
+
+// const BACKEND_URL = "http://localhost:3001/"
+// const FRONTEND_URL = "http://localhost:3000/"
 
 export { BACKEND_URL, FRONTEND_URL }
